50553d6deb9fbc5bdd04bdd92896aed1e0ce08f3
Alexandr <alexxaandr.m@gmail.com>
2026-07-14 15:37:54 +0500

    add TypeORM configuration and update app module for PostgreSQL integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CaseModule } from './case/case.module';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
+import { ConfigService } from '@nestjs/config';
+import { getTypeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [CaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: getTypeOrmConfig,
+      inject: [ConfigService],
+    }),
+    CaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
